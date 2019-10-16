23556a3f86ea0a1c0c6c317e54e1cb8d7a3ac6e8
daniel parra <danialepaco@hotmail.com>
2019-10-16 11:31:09 -0500

    framework added

---
+++ framework/framework.h
@@ -0,0 +1,19 @@
+//
+//  framework.h
+//  framework
+//
+//  Created by daniel parra on 10/16/19.
+//  Copyright © 2019 daniel parra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework.
+FOUNDATION_EXPORT double frameworkVersionNumber;
+
+//! Project version string for framework.
+FOUNDATION_EXPORT const unsigned char frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework/PublicHeader.h>
+
+
