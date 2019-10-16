16fe0f744e21bd505af6549424b9e0ea00e36830
daniel parra <danialepaco@hotmail.com>
2019-10-16 15:15:11 -0500

    changed framework name

---
+++ FrameworkDemo/FrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkDemo.h
+//  FrameworkDemo
+//
+//  Created by daniel parra on 10/16/19.
+//  Copyright © 2019 daniel parra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkDemo.
+FOUNDATION_EXPORT double FrameworkDemoVersionNumber;
+
+//! Project version string for FrameworkDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkDemo/PublicHeader.h>
+
+
---
+++ FrameworkDemo/FrameworkMethods.swift
@@ -0,0 +1,13 @@
+//
+//  FrameworkMethods.swift
+//  demo
+//
+//  Created by daniel parra on 10/16/19.
+//  Copyright © 2019 daniel parra. All rights reserved.
+//
+
+import Foundation
+
+func getMessage(_ greeting: String) -> String {
+    return greeting + "from a framework"
+}
