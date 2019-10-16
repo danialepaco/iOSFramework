8c31818d953a3bfe9a6db38f0ff56ac095b573d4
daniel parra <danialepaco@hotmail.com>
2019-10-16 11:08:21 -0500

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by daniel parra on 10/16/19.
+//  Copyright © 2019 daniel parra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
